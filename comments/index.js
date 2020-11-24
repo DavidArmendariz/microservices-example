ca6b2166a4f2400b95be425d3a410db423a69a20
David Armendariz <darmendariz1998@outlook.com>
2020-11-23 20:35:35 -0500

    Added logic to process events that were not processed

--- comments/index.js
+++ comments/index.js
@@ -36,10 +36,31 @@ app.post(
   })
 );
 
-app.post("/events", (req, res) => {
-  console.log("[comments] Event Received:", req.body.type);
-  res.send({});
-});
+app.post(
+  "/events",
+  asyncHandler(async (req, res) => {
+    console.log("[comments] Event Received:", req.body.type);
+    const { type, data } = req.body;
+    if (type === "CommentModerated") {
+      const { postId, id, status, content } = data;
+      const comments = commentsByPostId[postId];
+      const comment = comments.find((comment) => {
+        return comment.id === id;
+      });
+      comment.status = status;
+      await axios.post("http://localhost:4005/events", {
+        type: "CommentUpdated",
+        data: {
+          id,
+          status,
+          postId,
+          content,
+        },
+      });
+    }
+    res.send({});
+  })
+);
 
 app.use((err, req, res, next) => {
   console.error("[comments]", err.stack);
